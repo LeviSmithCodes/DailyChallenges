5e84c701512470cbe474fa458e7c3e3024391189
LeviSmithCodes <eyevel@gmail.com>
2020-02-26 17:35:31 -0700

    2.26.20

--- DailyChallenges.js
+++ DailyChallenges.js
@@ -42,4 +42,21 @@ function convertToDecimal2(arr) {
   return arr.map(x => parseFloat(x) / 100);
 }
 
-console.log(convertToDecimal2(["33%", "98.1%", "56.44%", "100%"]));
+// console.log(convertToDecimal2(["33%", "98.1%", "56.44%", "100%"]));
+
+// ------------------------------------------------
+// 2.26.20
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits. Your task is to create a function that takes a string and returns true if the PIN is valid and false if it's not.
+
+function validatePIN(pin) {
+  return (
+    (/\d{4}/.test(pin) || /\d{6}/.test(pin)) &&
+    (pin.length == 4 || pin.length == 6)
+  );
+}
+
+function validatePIN_optimized(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+}
+
+console.log(validatePIN_optimized("1234"));
