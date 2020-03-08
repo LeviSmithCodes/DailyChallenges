b1648467492934c7dd8c0cd671eecc4480eb7dcd
LeviSmithCodes <eyevel@gmail.com>
2020-03-08 17:02:48 -0600

    one

--- DailyChallenges.js
+++ DailyChallenges.js
@@ -415,5 +415,23 @@ function dailyStreak(arr) {
   return totalCount;
 }
 
-console.log(dailyStreak([true, true, false, true, true, true]));
+//console.log(dailyStreak([true, true, false, true, true, true]));
 // ➞ 3
+// ----------------------------------------
+// 3/8/20
+// Create a function that takes an array of numbers and returns an array where each number is the sum of itself + all previous numbers in the array.
+function cumulativeSum(arr) {
+  // create new array
+  let returnArr = [];
+  // create sum
+  let sum = 0;
+  // loop through array and add sum to new array
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    returnArr.push(sum);
+  }
+  return returnArr;
+}
+
+console.log(cumulativeSum([3, 3, -2, 408, 3, 3]));
+//➞ [3, 6, 4, 412, 415, 418]
